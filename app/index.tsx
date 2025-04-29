14990c7f929a92f54edb7bf2779fa8f188c5faca
GalYazdi <galyazdi11@gmail.com>
2025-04-29 17:36:05 +0300

    added nativecn library

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,30 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View, Text } from "react-native";
+import { Button } from "../components/Button";
 
-export default function Index() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <View
-    >
-      <Text className="text-2xl">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center gap-4">
+      <Text className="text-3xl font-bold mb-4">
+        NativeWind work
+      </Text>
+
+      <Button
+        labelClasses="text-pink-400" 
+
+        label={"Tap if Omer gay"}
+        onPress={() => {
+          setCount((c) => c + 1);
+          setClicked(true);
+        }}
+        variant="default"
+        size="default"
+      />
+
+      <Text className="text-3xl font-bold mb-4 text-pink-400">{count}</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}", // 👈 חובה כדי שיראה את המחלקות בקומפוננטות
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#000000", // blue
+        "primary-foreground": "#ffffff",
+        secondary: "#64748b", // gray
+        "secondary-foreground": "#ffffff",
+        destructive: "#ef4444", // orange
+        "destructive-foreground": "#ffffff",
+      },
+    },
   },
   plugins: [],
-}
+};
