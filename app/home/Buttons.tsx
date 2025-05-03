362b17676f62a490e8c8cf331918160176eb14ed
GalYazdi <galyazdi11@gmail.com>
2025-05-03 13:25:47 +0300

    updated some css

--- app/home/Buttons.tsx
+++ app/home/Buttons.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/Button";
 import { View } from "react-native";
 export default function Buttons() {
     return (
-        <View className="flex-row items-center w-full px-4 py-2 mt-20">
+        <View className="flex-row items-center justify-center gap-20 w-full px-4 py-2 mt-20">
             <Button
                 label={"הצג מיקום"}
                 onPress={() => {}}
--- app/home/HomePageHeader.tsx
+++ app/home/HomePageHeader.tsx
@@ -1,28 +1,32 @@
 // HomePageHeader.js
-import { View, Text, Image, Pressable } from "react-native";
+import { SafeAreaView, View, Text, Image, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "@/components/Button";
 import { User } from "lucide-react-native";
 
 export default function HomePageHeader() {
   return (
-    <View className="w-full flex-wrap flex-row items-center justify-between px-4 py-2 bg-gray-200 shadow-sm">
-      <Pressable className="w-16 h-16 rounded-full bg-black items-center justify-center flex-row gap-1">
-        {/* <User color="white" size={28} /> */}
-        <Text className="text-base text-white">Guest</Text>
-      </Pressable>
+    <SafeAreaView>
+      <View className="w-full flex-wrap flex-row items-center justify-between px-2 h-[55px] bg-gray-100 shadow-sm">
+        <Pressable className="w-12 h-12 ml-1 rounded-full bg-black items-center justify-center flex-row gap-1">
+          {/* <User color="white" size={28} /> */}
+          <Text className="text-sm text-white">Guest</Text>
+        </Pressable>
 
-      <View className="flex-row items-center space-x-2">
-        <View>
-          <Text className="text-2xl font-bold text-blue-700">החובש הר נוף</Text>
-          <Text className="text-base text-blue-700">ארגון ההצלה השכונתי</Text>
+        <View className="flex-row items-center space-x-2">
+          <View>
+            <Text className="text-2xl font-bold text-blue-700">
+              החובש הר נוף
+            </Text>
+            <Text className="text-base text-blue-700">ארגון ההצלה השכונתי</Text>
+          </View>
+          <Image
+            source={require("../../assets/images/logo.png")}
+            style={{ width: 45, height: 45 }}
+            resizeMode="contain"
+          />
         </View>
-        <Image
-          source={require("../../assets/images/logo.png")}
-          style={{ width: 64, height: 64 }}
-          resizeMode="contain"
-        />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
