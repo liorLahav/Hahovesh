91839c5514949bf1007ffaccd87b4ac0ea2a0a75
liorc8 <liorc1212@gmail.com>
2025-05-15 21:30:53 +0300

    move the NewEvent file to newEvent folder

--- app/newEvent/NewEvent.tsx
+++ app/newEvent/NewEvent.tsx
@@ -1,11 +1,10 @@
 
 import { ScrollView, Text, Alert, View } from 'react-native';
-import { ref, push, set, serverTimestamp } from 'firebase/database';
 
-import { realtimeDb } from '../FirebaseConfig';
 import formSchema from '../data/formSchema';
 import DynamicForm from '../components/DynamicForm';
-import { useState } from 'react'; 
+import { useState } from 'react';
+import { handleSubmit } from './newEvent/handleSubmit'; 
 
 import HomePageHeader from "./home/HomePageHeader";
 
@@ -13,23 +12,8 @@ export default function NewEventScreen() {
   const [formKey, setFormKey] = useState(0);   
 
   /** Push a new event object to RTDB */
-  const handleSubmit = async (values: Record<string, string>) => {
-    try {
-      const node = push(ref(realtimeDb, 'events'));
-
-      await set(node, {
-        ...values,
-        createdAt: serverTimestamp(),
-      });
-
-      console.log('✅ Saved event:', values);
-      Alert.alert('Success', 'Event saved to database ✅');
-
-      setFormKey(k => k + 1);
-    } catch (err: any) {
-      console.error('❌ RTDB error:', err);
-      Alert.alert('Error', err?.message ?? 'Could not save the event');
-    }
+  const onSubmit = (values: Record<string, string>) => {
+    handleSubmit(values, () => setFormKey(k => k + 1));
   };
 
   return (
