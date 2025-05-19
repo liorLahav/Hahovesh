2d038dffbabc6489b28e0a21bf18a64f37484489
GalYazdi <galyazdi11@gmail.com>
2025-05-19 14:02:51 +0300

    dynamic comp to display more info about event

--- app/events/[id].tsx
+++ app/events/[id].tsx
@@ -7,7 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import DetailsHeader from "./DetailsHeader";
 
 export default function EventDetails() {
-  const { id } = useLocalSearchParams(); // id = createdAt
+  const { id } = useLocalSearchParams();
   const [event, setEvent] = useState<Event | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -44,27 +44,42 @@ export default function EventDetails() {
   }
 
   const details = [
-    { label: "שם האירוע", value: event?.anamnesis },
-    { label: "תאריך", value: event?.createdAt },
-    { label: "מיקום", value: event?.location },
-    { label: "תיאור", value: event?.description },
-    { label: "סטטוס", value: event?.status },
-    { label: "סוג אירוע", value: event?.eventType },
-    { label: "אחריות", value: event?.responsibility },
-]
+    { label: "סוג האירוע", value: event?.anamnesis },
+    { label: "תאריך", value: event?.createdAt.toString() },
+    { label: "רחוב", value: event?.street },
+    { label: "מספר בית", value: event?.house_number },
+    { label: "פרטי דירה", value: event?.apartment_details },
+    { label: "מיקום", value: event?.location_type },
+    { label: "קוד רפואי", value: event?.medical_code },
+    { label: "קוד הזנקה", value: event?.haznk_code },
+    { label: "מוקד מקבל", value: event?.recipient },
+    { label: "דחיפות", value: event?.urgency },
+    { label: "טלפון פונה", value: event?.phone_number1 },
+    { label: "טלפון נוסף", value: event?.phone_number2 },
+    { label: "גיל פונה", value: event?.patient_age },
+    { label: "שם פונה", value: event?.patient_name },
+    { label: "מין פונה", value: event?.patient_sex },
+    { label: "מודיע", value: event?.informat_location },
+  ];
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1">
       <DetailsHeader />
       <ScrollView
         contentContainerStyle={{ padding: 20, paddingBottom: 100 }}
-        className="bg-white"
+        className=" bg-blue-50"
       >
-
-        {Object.entries(event).map(([key, value]) => (
-          <View key={key} className="mb-4 border-b border-gray-200 pb-2">
-            <Text className="text-sm text-gray-500 text-right">{key}</Text>
-            <Text className="text-lg text-right text-gray-800">{value}</Text>
+        {details.map((detail) => (
+          <View
+            key={detail.label}
+            className="mb-4 border-b border-gray-200 pb-2"
+          >
+            <Text className="text-sm text-gray-500 text-right">
+              {detail.label}
+            </Text>
+            <Text className="text-lg text-right text-gray-800">
+              {detail.value == "" ? "-" : detail.value}
+            </Text>
           </View>
         ))}
       </ScrollView>
@@ -72,4 +87,93 @@ export default function EventDetails() {
   );
 }
 
+// V2
+// // app/events/[id].tsx
+// import { View, Text, ScrollView } from "react-native";
+// import { useLocalSearchParams } from "expo-router";
+// import { useEffect, useState } from "react";
+// import { subscribeToEvents, Event } from "@/services/events";
+// import { SafeAreaView } from "react-native-safe-area-context";
+// import DetailsHeader from "./DetailsHeader";
 
+// export default function EventDetails() {
+//   const { id } = useLocalSearchParams();
+//   const [event, setEvent] = useState<Event | null>(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const unsubscribe = subscribeToEvents((events, error) => {
+//       if (error || !events) {
+//         setEvent(null);
+//         setLoading(false);
+//         return;
+//       }
+
+//       const found = events.find((e) => e.id === id);
+//       setEvent(found || null);
+//       setLoading(false);
+//     });
+
+//     return () => unsubscribe();
+//   }, [id]);
+
+//   if (loading) {
+//     return (
+//       <View className="flex-1 items-center justify-center">
+//         <Text>טוען פרטי אירוע...</Text>
+//       </View>
+//     );
+//   }
+
+//   if (!event) {
+//     return (
+//       <View className="flex-1 items-center justify-center">
+//         <Text>אירוע לא נמצא</Text>
+//       </View>
+//     );
+//   }
+
+//   const details = [
+//     { label: "סוג האירוע", value: event?.anamnesis },
+//     { label: "תאריך", value: event?.createdAt.toString() },
+//     { label: "רחוב", value: event?.street },
+//     { label: "מספר בית", value: event?.house_number },
+//     { label: "פרטי דירה", value: event?.apartment_details },
+//     { label: "מיקום", value: event?.location_type },
+//     { label: "קוד רפואי", value: event?.medical_code },
+//     { label: "קוד הזנקה", value: event?.haznk_code },
+//     { label: "מוקד מקבל", value: event?.recipient },
+//     { label: "דחיפות", value: event?.urgency },
+//     { label: "טלפון פונה", value: event?.phone_number1 },
+//     { label: "טלפון נוסף", value: event?.phone_number2 },
+//     { label: "גיל פונה", value: event?.patient_age },
+//     { label: "שם פונה", value: event?.patient_name },
+//     { label: "מין פונה", value: event?.patient_sex },
+//     { label: "מודיע", value: event?.informat_location },
+
+//   ];
+
+//   return (
+//     <SafeAreaView className="flex-1">
+//       <DetailsHeader />
+//       <ScrollView
+//         contentContainerStyle={{ padding: 20, paddingBottom: 100 }}
+//         className=" bg-blue-50"
+//       >
+//         {details.map((detail) => (
+//           <View
+//             key={detail.label}
+//             className="mb-4 border-b border-gray-200 pb-2"
+//           >
+//             <Text className="text-sm text-gray-500 text-right">
+//               {detail.label}
+//             </Text>
+//             <Text className="text-lg text-right text-gray-800">
+//               {detail.value}
+//             </Text>
+//           </View>
+//         ))}
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
