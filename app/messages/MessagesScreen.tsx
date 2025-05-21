ec0ee750e767e38c5a2eccbd383446e0152e5829
GalYazdi <galyazdi11@gmail.com>
2025-05-21 11:15:30 +0300

    hero section on process

---
+++ app/messages/MessagesScreen.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from 'react-native';
+import MessagesHeader from './MessagesHeader';
+
+export default function MessagesScreen() {
+    return (
+        <SafeAreaView>
+        <MessagesHeader />
+        {/* תוכן נוסף */}
+        </SafeAreaView>
+    );
+}
