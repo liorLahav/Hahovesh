052dad74a08d792c469ebb1719cf01a8ef93901f
GalYazdi <galyazdi11@gmail.com>
2025-06-01 22:41:31 +0300

    Adds delete functionality with confirmation dialog
    Introduces a delete message feature with a confirmation alert, including a visual indicator (trash icon) for user interaction. Updates UI components for improved usability and adds `StatusBar` customization. Enhances code structure by importing necessary dependencies.

--- app/messages/MessagesScreen.tsx
+++ app/messages/MessagesScreen.tsx
@@ -1,15 +1,27 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Pressable, Alert } from "react-native";
 import { useEffect, useState } from "react";
-import { markMessagesAsRead } from "@/services/messages";
+import {
+  deleteMessage,
+  markMessagesAsRead,
+  Message,
+} from "@/services/messages";
 import { useRolesContext } from "@/hooks/RolesContext";
 import { useMessages } from "@/hooks/MessagesContext";
-import { deleteAllMessages } from "@/services/messages";
+import { StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MessagesScreen() {
   const { roles, rolesLoading } = useRolesContext();
   const { messages, loadingMessages } = useMessages();
-
   const userId = "abc";
+
+  const handleDeleteMessage = async (msgId: string) => {
+    try {
+      await deleteMessage(msgId);
+    } catch (err) {
+      console.error("שגיאה במחיקת הודעה:", err);
+    }
+  };
 
   useEffect(() => {
     if (messages.length && userId) {
@@ -46,41 +58,65 @@ export default function MessagesScreen() {
   const visibleMessages = getVisibleMessages() || [];
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 16, paddingBottom: 80 }}>
-      {visibleMessages.length === 0 ? (
-        <Text className="text-center text-blue-700 text-lg mt-8">
-          אין הודעות להצגה
-        </Text>
-      ) : (
-        visibleMessages.reverse().map((msg) => (
-          <View
-            key={msg.message_id}
-            className="mb-6 bg-blue-50 p-4 rounded-xl shadow-sm border border-gray-200 flex flex-col items-end"
-          >
-            <View className="flex flex-row justify-between items-end mb-1">
-              <Text className="text-right text-sm text-gray-500 mb-1">
-                {msg.date} {msg.time}
+    <>
+      <StatusBar backgroundColor="black" />
+      <ScrollView contentContainerStyle={{ padding: 16, paddingBottom: 80 }}>
+        {visibleMessages.length === 0 ? (
+          <Text className="text-center text-blue-700 text-lg mt-8">
+            אין הודעות להצגה
+          </Text>
+        ) : (
+          visibleMessages.reverse().map((msg) => (
+            <View
+              key={msg.message_id}
+              className="mb-6 bg-blue-50 p-4 rounded-xl shadow-sm border border-gray-200 flex flex-col "
+            >
+              <View className="flex flex-row justify-between items-center mb-1">
+                <Pressable
+                  onPress={() => {
+                    Alert.alert(
+                      "האם אתה בטוח?",
+                      "לאחר המחיקה לא תוכל לשחזר הודעה",
+                      [
+                        {
+                          text: "ביטול",
+                          style: "cancel",
+                        },
+                        {
+                          text: "אישור",
+                          onPress: () => handleDeleteMessage(msg.message_id),
+                        },
+                      ]
+                    );
+                  }}
+                >
+                  <Ionicons name="trash-outline" size={22} color="red" />
+                </Pressable>
+
+                <Text className="text-right text-sm text-gray-500 mb-1">
+                  {msg.date} {msg.time}
+                </Text>
+              </View>
+
+              <Text className="text-right text-sm font-bold text-gray-800 mb-2">
+                {msg.distribution_by_role === "All"
+                  ? "[כולם]"
+                  : msg.distribution_by_role === "Dispatcher"
+                  ? "[מוקדן]"
+                  : msg.distribution_by_role === "Admin"
+                  ? "[מנהל]"
+                  : msg.distribution_by_role === "Volunteers"
+                  ? "[חובשים]"
+                  : null}
+              </Text>
+
+              <Text className="text-right text-gray-800 leading-relaxed">
+                {msg.message_description}
               </Text>
             </View>
-
-            <Text className="text-right text-sm font-bold text-gray-800 mb-2">
-              {msg.distribution_by_role === "All"
-                ? "[כולם]"
-                : msg.distribution_by_role === "Dispatcher"
-                ? "[מוקדן]"
-                : msg.distribution_by_role === "Admin"
-                ? "[מנהל]"
-                : msg.distribution_by_role === "Volunteers"
-                ? "[חובשים]"
-                : null}
-            </Text>
-
-            <Text className="text-right text-gray-800 leading-relaxed">
-              {msg.message_description}
-            </Text>
-          </View>
-        ))
-      )}
-    </ScrollView>
+          ))
+        )}
+      </ScrollView>
+    </>
   );
 }
